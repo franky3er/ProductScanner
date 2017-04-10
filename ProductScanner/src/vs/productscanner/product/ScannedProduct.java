fba7e08b360c5e6ac985962d678090d239678f9c
Frank <Frank@10.168.140.180>
2017-04-11 00:28:05 +0200

    feature/ScannedProduct - include Product interface

--- ProductScanner/src/vs/productscanner/product/ScannedProduct.java
+++ ProductScanner/src/vs/productscanner/product/ScannedProduct.java
@@ -1,5 +1,23 @@
 package vs.productscanner.product;
 
-public class ScannedProduct {
+public class ScannedProduct implements Product {
 
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUnit() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getStateAsString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
