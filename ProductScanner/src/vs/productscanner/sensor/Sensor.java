c67ae1ff42ca1791eccd9a5422ea6e1b88581363
franky3er <franky3er@localhost.localdomain>
2017-04-11 09:31:47 +0200

    feature/Sensor - expand sensor with ScanOption

--- ProductScanner/src/vs/productscanner/sensor/Sensor.java
+++ ProductScanner/src/vs/productscanner/sensor/Sensor.java
@@ -1,5 +1,18 @@
 package vs.productscanner.sensor;
 
-public class Sensor {
+import vs.productscanner.sensor.scanoption.ScanOption;
 
+public class Sensor {
+	private ScanOption scanOption;
+	
+	public Sensor(ScanOption scanOption){
+		this.setScanOption(scanOption);
+	}
+	
+	public ScanOption getScanOption() {
+		return scanOption;
+	}
+	public void setScanOption(ScanOption scanOption) {
+		this.scanOption = scanOption;
+	}
 }
