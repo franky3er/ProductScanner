08fac6a05a979df57b839ff3dad885835dd8162b
franky3er <franky3er@localhost.localdomain>
2017-04-11 08:54:58 +0200

    feature/Sensor - modify scan method

--- ProductScanner/src/vs/productscanner/sensor/scanoption/ScanOption.java
+++ ProductScanner/src/vs/productscanner/sensor/scanoption/ScanOption.java
@@ -1,5 +1,8 @@
 package vs.productscanner.sensor.scanoption;
 
+import vs.productscanner.product.Product;
+import vs.productscanner.product.ScannedProduct;
+
 public interface ScanOption {
-	String scan();
+	ScannedProduct scan(Product product);
 }
