fa6395dcf2c70e6da04a5a5ac8377d406c0ef6d4
franky3er <franky3er@localhost.localdomain>
2017-04-12 11:19:36 +0200

    feature/transmitter - create UDPTransmit

---
+++ ProductScanner/src/vs/productscanner/transmitter/transmitoption/udp/UDPTransmit.java
@@ -0,0 +1,14 @@
+package vs.productscanner.transmitter.transmitoption.udp;
+
+import vs.productscanner.product.ScannedProduct;
+import vs.productscanner.transmitter.transmitoption.TransmitOption;
+
+public class UDPTransmit implements TransmitOption {
+
+	@Override
+	public void transmit(ScannedProduct scannedProduct) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
