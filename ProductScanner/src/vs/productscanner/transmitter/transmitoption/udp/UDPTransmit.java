8d97bebc31b11a0588d11d0dc6f0b65b23cec23c
franky3er <franky3er@localhost.localdomain>
2017-04-12 11:49:54 +0200

    feature/transmitter - implement transmit UDPTransmit

--- ProductScanner/src/vs/productscanner/transmitter/transmitoption/udp/UDPTransmit.java
+++ ProductScanner/src/vs/productscanner/transmitter/transmitoption/udp/UDPTransmit.java
@@ -1,29 +1,48 @@
 package vs.productscanner.transmitter.transmitoption.udp;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 import vs.productscanner.product.ScannedProduct;
 import vs.productscanner.transmitter.transmitoption.TransmitOption;
 
 public class UDPTransmit implements TransmitOption {
 	
-	private String destinationIP;
+	private DatagramSocket clientSocket;	
+	private InetAddress destinationIP;
 	private int destinationPort;
 	
-	public UDPTransmit(String destinationIP, int destinationPort) {
+	public UDPTransmit(InetAddress destinationIP, int destinationPort) {
 		this.destinationIP = destinationIP;
 		this.destinationPort = destinationPort;
+		try {
+			this.clientSocket = new DatagramSocket();
+		} catch (SocketException e) {
+			System.err.println("ERROR : Could not create Datagram Socket");
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
 	public void transmit(ScannedProduct scannedProduct) {
-		// TODO Auto-generated method stub
+		String sendData = scannedProduct.getStateAsString();
+		try {
+			clientSocket.send(new DatagramPacket(sendData.getBytes(), sendData.getBytes().length, destinationIP, destinationPort));
+		} catch (IOException e) {
+			System.err.println("ERROR : Failed to send data: " + sendData);
+			e.printStackTrace();
+		}
 		
 	}
 
-	public String getDestinationIP() {
+	public InetAddress getDestinationIP() {
 		return destinationIP;
 	}
 
-	public void setDestinationIP(String destinationIP) {
+	public void setDestinationIP(InetAddress destinationIP) {
 		this.destinationIP = destinationIP;
 	}
 
