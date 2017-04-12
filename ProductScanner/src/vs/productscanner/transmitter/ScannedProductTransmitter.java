c08bd8b063036cae5f9369f551774b18ae9fb429
franky3er <franky3er@localhost.localdomain>
2017-04-12 13:58:52 +0200

    feature/transmitter - complete

--- ProductScanner/src/vs/productscanner/transmitter/ScannedProductTransmitter.java
+++ ProductScanner/src/vs/productscanner/transmitter/ScannedProductTransmitter.java
@@ -1,5 +1,39 @@
 package vs.productscanner.transmitter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import vs.productscanner.product.ScannedProduct;
+import vs.productscanner.transmitter.transmitoption.TransmitOption;
+
 public class ScannedProductTransmitter {
+	private List<TransmitOption> transmitOptions;
 	
+	public ScannedProductTransmitter() {
+		transmitOptions = new ArrayList<TransmitOption>();
+	}
+	
+	public ScannedProductTransmitter(List<TransmitOption> transmitOptions) {
+		this.transmitOptions = transmitOptions;
+	}
+	
+	public void transmit(List<ScannedProduct> scannedProducts){
+		for(ScannedProduct scannedProduct : scannedProducts) {
+			this.transmit(scannedProduct);
+		}
+	}
+	
+	public void transmit(ScannedProduct scannedProduct) {
+		for(TransmitOption transmitOption : this.transmitOptions) {
+			transmitOption.transmit(scannedProduct);
+		}
+	}
+	
+	public void addTransmitOption(TransmitOption transmitOption) {
+		this.transmitOptions.add(transmitOption);
+	}
+	
+	public void removeTransmitOption(TransmitOption transmitOption) {
+		this.transmitOptions.remove(transmitOption);
+	}
 }
