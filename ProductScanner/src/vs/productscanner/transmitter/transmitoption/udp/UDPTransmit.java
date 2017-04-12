79ce7261972ce07d0746a866e1de5f3086cea356
franky3er <franky3er@localhost.localdomain>
2017-04-12 11:28:42 +0200

    feature/transmitter - create attributes UDPTransmit

--- ProductScanner/src/vs/productscanner/transmitter/transmitoption/udp/UDPTransmit.java
+++ ProductScanner/src/vs/productscanner/transmitter/transmitoption/udp/UDPTransmit.java
@@ -4,11 +4,35 @@ import vs.productscanner.product.ScannedProduct;
 import vs.productscanner.transmitter.transmitoption.TransmitOption;
 
 public class UDPTransmit implements TransmitOption {
-
+	
+	private String destinationIP;
+	private int destinationPort;
+	
+	public UDPTransmit(String destinationIP, int destinationPort) {
+		this.destinationIP = destinationIP;
+		this.destinationPort = destinationPort;
+	}
+	
 	@Override
 	public void transmit(ScannedProduct scannedProduct) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public String getDestinationIP() {
+		return destinationIP;
+	}
+
+	public void setDestinationIP(String destinationIP) {
+		this.destinationIP = destinationIP;
+	}
+
+	public int getDestinationPort() {
+		return destinationPort;
+	}
+
+	public void setDestinationPort(int destinationPort) {
+		this.destinationPort = destinationPort;
 	}
 
 }
